be12d0982937e015c9eacdd9cdbb8e543c301698
Botty Sundaragopalan <prasanna.kumar.botty.sundaragopalan@autozone.com>
2025-03-01 14:19:23 -0600

    updated the BaseHelper file

--- src/main/java/utilities/BaseHelper.java
+++ src/main/java/utilities/BaseHelper.java
@@ -6,18 +6,18 @@ import java.util.Properties;
 public class BaseHelper {
 	
 	static String filePath = "C:\\Users\\blaxm\\workspace\\com.tvtProject\\src\\main\\config.properties";
-	Properties property;
+	Properties prop;
 	
 	public  Properties readPropertyFile(){
 		
 		try{
 			FileInputStream fis = new FileInputStream(filePath);
-			property = new Properties();
-			property.load(fis);
+			prop = new Properties();
+			prop.load(fis);
 		}catch(Exception e){
 			e.printStackTrace();
 		}
-		return property;
+		return prop;
 	}
 
 }
